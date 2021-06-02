10a9c6ac435d8ee058da5d89cad08e67ce7699ec
Cesar <elisma12-12@hotmail.com>
2021-06-02 17:16:09 -0400

    Calculadora de intereses

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtCapital = new System.Windows.Forms.TextBox();
+            this.txtTiempo = new System.Windows.Forms.TextBox();
+            this.txtTasa = new System.Windows.Forms.TextBox();
+            this.txtInteres = new System.Windows.Forms.TextBox();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCapital = new System.Windows.Forms.Label();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.lblTasa = new System.Windows.Forms.Label();
+            this.lblInteres = new System.Windows.Forms.Label();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnMinimizar = new System.Windows.Forms.PictureBox();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(193, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp3.Properties.Resources.images;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 150);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(193, 147);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // txtCapital
+            // 
+            this.txtCapital.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txtCapital.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCapital.ForeColor = System.Drawing.Color.DimGray;
+            this.txtCapital.Location = new System.Drawing.Point(435, 95);
+            this.txtCapital.Name = "txtCapital";
+            this.txtCapital.Size = new System.Drawing.Size(228, 13);
+            this.txtCapital.TabIndex = 1;
+            this.txtCapital.Text = "CAPITAL";
+            this.txtCapital.Enter += new System.EventHandler(this.txtCapital_Enter);
+            this.txtCapital.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCapital_KeyPress);
+            this.txtCapital.Leave += new System.EventHandler(this.txtCapital_Leave);
+            // 
+            // txtTiempo
+            // 
+            this.txtTiempo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txtTiempo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTiempo.ForeColor = System.Drawing.Color.DimGray;
+            this.txtTiempo.Location = new System.Drawing.Point(435, 150);
+            this.txtTiempo.Name = "txtTiempo";
+            this.txtTiempo.Size = new System.Drawing.Size(228, 13);
+            this.txtTiempo.TabIndex = 2;
+            this.txtTiempo.Text = "TIEMPO";
+            this.txtTiempo.Enter += new System.EventHandler(this.txtTiempo_Enter);
+            this.txtTiempo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTiempo_KeyPress);
+            this.txtTiempo.Leave += new System.EventHandler(this.txtTiempo_Leave);
+            // 
+            // txtTasa
+            // 
+            this.txtTasa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txtTasa.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTasa.ForeColor = System.Drawing.Color.DimGray;
+            this.txtTasa.Location = new System.Drawing.Point(435, 207);
+            this.txtTasa.Name = "txtTasa";
+            this.txtTasa.Size = new System.Drawing.Size(228, 13);
+            this.txtTasa.TabIndex = 3;
+            this.txtTasa.Text = "TASA";
+            this.txtTasa.Enter += new System.EventHandler(this.txtTasa_Enter);
+            this.txtTasa.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTasa_KeyPress);
+            this.txtTasa.Leave += new System.EventHandler(this.txtTasa_Leave);
+            // 
+            // txtInteres
+            // 
+            this.txtInteres.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txtInteres.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtInteres.Enabled = false;
+            this.txtInteres.ForeColor = System.Drawing.Color.DimGray;
+            this.txtInteres.Location = new System.Drawing.Point(435, 322);
+            this.txtInteres.Name = "txtInteres";
+            this.txtInteres.Size = new System.Drawing.Size(228, 13);
+            this.txtInteres.TabIndex = 4;
+            this.txtInteres.Text = "INTERES";
+            this.txtInteres.Enter += new System.EventHandler(this.txtInteres_Enter);
+            this.txtInteres.Leave += new System.EventHandler(this.txtInteres_Leave);
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txtMonto.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMonto.Enabled = false;
+            this.txtMonto.ForeColor = System.Drawing.Color.DimGray;
+            this.txtMonto.Location = new System.Drawing.Point(435, 379);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(228, 13);
+            this.txtMonto.TabIndex = 5;
+            this.txtMonto.Text = "MONTO";
+            this.txtMonto.Enter += new System.EventHandler(this.txtMonto_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(408, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calcular Interes Compuesto";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblCapital
+            // 
+            this.lblCapital.AutoSize = true;
+            this.lblCapital.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.lblCapital.ForeColor = System.Drawing.Color.LightGray;
+            this.lblCapital.Location = new System.Drawing.Point(247, 96);
+            this.lblCapital.Name = "lblCapital";
+            this.lblCapital.Size = new System.Drawing.Size(54, 13);
+            this.lblCapital.TabIndex = 1;
+            this.lblCapital.Text = "CAPITAL:";
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.ForeColor = System.Drawing.Color.LightGray;
+            this.lblTiempo.Location = new System.Drawing.Point(247, 151);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(51, 13);
+            this.lblTiempo.TabIndex = 2;
+            this.lblTiempo.Text = "TIEMPO:";
+            // 
+            // lblTasa
+            // 
+            this.lblTasa.AutoSize = true;
+            this.lblTasa.ForeColor = System.Drawing.Color.LightGray;
+            this.lblTasa.Location = new System.Drawing.Point(247, 208);
+            this.lblTasa.Name = "lblTasa";
+            this.lblTasa.Size = new System.Drawing.Size(88, 13);
+            this.lblTasa.TabIndex = 3;
+            this.lblTasa.Text = "TASA INTERES:";
+            // 
+            // lblInteres
+            // 
+            this.lblInteres.AutoSize = true;
+            this.lblInteres.ForeColor = System.Drawing.Color.LightGray;
+            this.lblInteres.Location = new System.Drawing.Point(247, 323);
+            this.lblInteres.Name = "lblInteres";
+            this.lblInteres.Size = new System.Drawing.Size(57, 13);
+            this.lblInteres.TabIndex = 4;
+            this.lblInteres.Text = "INTERES:";
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.ForeColor = System.Drawing.Color.LightGray;
+            this.lblMonto.Location = new System.Drawing.Point(247, 380);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(50, 13);
+            this.lblMonto.TabIndex = 6;
+            this.lblMonto.Text = "MONTO:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnCalcular.FlatAppearance.BorderSize = 0;
+            this.btnCalcular.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnCalcular.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.ForeColor = System.Drawing.Color.LightGray;
+            this.btnCalcular.Location = new System.Drawing.Point(435, 262);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.ForeColor = System.Drawing.Color.LightGray;
+            this.btnCancelar.Location = new System.Drawing.Point(588, 262);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.Text = "Limpiar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Image = global::WindowsFormsApp3.Properties.Resources.Minimize_Icon;
+            this.btnMinimizar.Location = new System.Drawing.Point(751, 0);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(21, 15);
+            this.btnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMinimizar.TabIndex = 11;
+            this.btnMinimizar.TabStop = false;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Image = global::WindowsFormsApp3.Properties.Resources.Close_Icon;
+            this.btnCerrar.Location = new System.Drawing.Point(778, 0);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(21, 15);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnCerrar.TabIndex = 10;
+            this.btnCerrar.TabStop = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMinimizar);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblMonto);
+            this.Controls.Add(this.lblInteres);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.lblTasa);
+            this.Controls.Add(this.txtInteres);
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.txtTasa);
+            this.Controls.Add(this.lblCapital);
+            this.Controls.Add(this.txtTiempo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCapital);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtCapital;
+        private System.Windows.Forms.TextBox txtTiempo;
+        private System.Windows.Forms.TextBox txtTasa;
+        private System.Windows.Forms.TextBox txtInteres;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCapital;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.Label lblTasa;
+        private System.Windows.Forms.Label lblInteres;
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox btnCerrar;
+        private System.Windows.Forms.PictureBox btnMinimizar;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCapital_Enter(object sender, EventArgs e)
+        {
+            if (txtCapital.Text=="CAPITAL")
+            {
+                txtCapital.Text = "";
+                txtCapital.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtCapital_Leave(object sender, EventArgs e)
+        {
+            if(txtCapital.Text=="")
+            {
+                txtCapital.Text = "CAPITAL";
+                txtCapital.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void txtTiempo_Enter(object sender, EventArgs e)
+        {
+            if (txtTiempo.Text == "TIEMPO")
+            {
+                txtTiempo.Text = "";
+                txtTiempo.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtTiempo_Leave(object sender, EventArgs e)
+        {
+            if (txtTiempo.Text == "")
+                txtTiempo.Text = "TIEMPO";
+            txtTiempo.ForeColor = Color.DimGray;
+            {
+
+            }
+        }
+
+        private void txtTasa_Enter(object sender, EventArgs e)
+        {
+            if(txtTasa.Text=="TASA")
+            {
+                txtTasa.Text = "";
+                txtTasa.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtTasa_Leave(object sender, EventArgs e)
+        {
+            if(txtTasa.Text=="")
+            {
+                txtTasa.Text = "TASA";
+                txtTasa.ForeColor = Color.DimGray;
+
+            }
+        }
+
+        private void txtInteres_Enter(object sender, EventArgs e)
+        {
+            if (txtInteres.Text == "INTERES")
+            {
+                txtInteres.Text="";
+                txtInteres.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtInteres_Leave(object sender, EventArgs e)
+        {
+            if (txtInteres.Text=="")
+            {
+                txtInteres.Text = "INTERES";
+                txtInteres.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void txtMonto_Enter(object sender, EventArgs e)
+        {
+            if(txtMonto.Text=="MONTO")
+            {
+                txtMonto.Text = "";
+                txtMonto.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double capital, r, t;
+            double interes, monto;
+
+            capital = double.Parse(txtCapital.Text);
+            r = double.Parse(txtTasa.Text);
+            t = double.Parse(txtTiempo.Text);
+
+            //Proceso de datos 
+
+            monto = Math.Pow((1 + r), t) * capital;
+            interes = monto - capital;
+
+            //salida de informacion o datos
+            txtMonto.Text = Convert.ToString(monto);
+            txtInteres.Text = Convert.ToString(interes);
+
+
+            
+
+
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtCapital.Text = "";
+            txtTiempo.Text = "";
+            txtTasa.Text = "";
+            txtInteres.Text = "";
+            txtMonto.Text = "";
+        }
+
+        private void txtCapital_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                txtTiempo.Focus();
+            }
+        }
+
+        private void txtTiempo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                txtTasa.Focus();
+            }
+
+        }
+
+        private void txtTasa_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                btnCalcular.PerformClick();
+                btnCancelar.Focus();
+            }
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+
+        }
+
+
+    }
+}
